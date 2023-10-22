67121ffb3780814a2b374b11d7488b71a14cd3f3
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-10-23 00:33:58 +0200

    Add files via upload

---
+++ Ponavljanje 9/Ponavljanje 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz = { 7,9,8,3,4,2,0 };
+
+            Array.Sort(niz);
+            foreach (int i in niz)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
